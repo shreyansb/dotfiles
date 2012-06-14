737e54e9ce119f194df34920c324f661aa7ad155
Shreyans Bhansali <shreyans@venmo.com>
2012-06-13 21:31:14 -0400

    added setup script and solarized colors for iterm2

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd ~
+ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/vim/vimrc/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/inputrc/inputrc ~/.inputrc
+ln -s ~/dotfiles/ack/ackrc ~/.ackrc
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
