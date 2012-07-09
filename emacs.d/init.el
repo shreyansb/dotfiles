e9f9468d217dde8d2b35575ab9a77683af2ddfaa
Shreyans Bhansali <shreyans@venmo.com>
2012-07-08 23:36:49 -0400

    emacs stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,3 +2,6 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/org-contrib/")
+(require 'org-checklist)
