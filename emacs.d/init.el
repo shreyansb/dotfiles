d239100ca18a83413e7a880e6918873c906a8597
Shreyans Bhansali <shreyans@venmo.com>
2012-09-16 23:29:05 -0400

    go stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,3 +5,9 @@
 
 (add-to-list 'load-path "~/.emacs.d/org-contrib/")
 (require 'org-checklist)
+
+;; put backup files in the backup directory
+(setq backup-directory-alist
+	  `((".*" . "~/.emacs_backups")))
+(setq auto-save-file-name-transforms
+	  `((".*" "~/.emacs_backups" t)))
